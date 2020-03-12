00c1a1df0db3b3913ac9b165d52845bd4fee6d7a
veteran233 <a1328906205@live.com>
2020-03-12 16:18:56 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.IO;
 
 namespace qndxx
 {
