9df14324ed101627ee9d519117197d7fa838129f
veteran233 <a1328906205@live.com>
2020-03-25 21:53:28 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
 
 namespace qndxx
 {
@@ -16,13 +19,28 @@ namespace qndxx
             InitializeComponent();
         }
 
+        //private void crawler()
+        //{
+        //    ScriptEngine engine = Python.CreateEngine();
+        //    ScriptScope scope = engine.CreateScope();
+
+        //    //var t = engine.GetSearchPaths();
+
+        //    scope.SetVariable("ans", 0);
+
+        //    ScriptSource source = engine.CreateScriptSourceFromFile(@"pachong.py");
+        //    source.Execute(scope);
+
+        //    var result = scope.GetVariable("ans");
+        //}
+
         private void button1_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == "" || textBox2.Text == "")
                 MessageBox.Show("输入框不能为空");
             else
             {
-                string filename = "http://sv.cyol.com/h5mv/" + int.Parse(textBox1.Text) + "dxx" + int.Parse(textBox2.Text) + ".mp4";
+                string filename = "http://dxx.wwwtop.top/dxx_video?a=" + textBox1.Text + "&b=" + textBox2.Text + "&c=2&d=1";
 
                 System.Diagnostics.Process.Start(filename);
             }
