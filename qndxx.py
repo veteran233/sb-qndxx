d26acdc353d12a378bfee936ef64b5e40eced656
veteran233 <a1328906205@live.com>
2020-04-01 20:58:39 +0800

    Update qndxx.py

--- qndxx.py
+++ qndxx.py
@@ -1,30 +1,25 @@
 import requests
-import webbrowser
+from webbrowser import *
 from bs4 import BeautifulSoup as bs
 
-s=input()
-a,b=s.split(' ')
+h_code=requests.get("http://news.cyol.com/node_67071.htm")
 
-#http://h5.cyol.com/special/daxuexi/daxuexi8z7/m.html?z=200
+s_code = bs(h_code.content, "lxml")
+
+code = s_code.find('a','transition')
+code = code.get("href")
+code = code.split('/')
+
+code = code[5]
 
 html=requests.get(
-    "http://h5.cyol.com/special/daxuexi/daxuexi"+a+"a"+b+"/m.html?z=200",
+    "http://h5.cyol.com/special/daxuexi/"+code+"/m.html?z=200",
     {"User-Agent" : "Mozilla/5.0 (Linux; Android 6.0.1; MuMu Build/V417IR; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36"}
     )
-
-c=1
-while (html.status_code==404):
-    if (c+97>122):
-        raise Exception("WTF?!")
-    html=requests.get(
-    "http://h5.cyol.com/special/daxuexi/daxuexi"+a+chr(97+c)+b+"/m.html?z=200",
-    {"User-Agent" : "Mozilla/5.0 (Linux; Android 6.0.1; MuMu Build/V417IR; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36"}
-    )
-    c=c+1
 
 x = bs(html.content,"lxml")
 
 url = x.find("video")
 url = url.get("src")
 
-webbrowser.open(url)
+open(url)
