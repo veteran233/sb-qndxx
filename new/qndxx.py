d12bd2124fe27aacb7b40d89b61f67b9ad470d99
veteran233 <a1328906205@live.com>
2020-04-09 23:51:23 +0800

    Update qndxx.py

--- new/qndxx.py
+++ new/qndxx.py
@@ -4,22 +4,13 @@ from bs4 import BeautifulSoup as bs
 
 h_code = get("http://news.cyol.com/node_67071.htm")
 
-s_code = bs(h_code.content, "lxml")
-
-code = s_code.find('a', 'transition')
-code = code.get("href")
-code = code.split('/')
-
-code = code[5]
+code = bs(h_code.content, "lxml").find('a', 'transition').get("href").split('/')[5]
 
 html = get(
     "http://h5.cyol.com/special/daxuexi/" + code + "/m.html?z=200",
     {"User-Agent" : "Mozilla/5.0 (Linux; Android 6.0.1; MuMu Build/V417IR; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36"}
     )
 
-x = bs(html.content, "lxml")
-
-url = x.find("video")
-url = url.get("src")
+url = bs(html.content, "lxml").find("video").get("src")
 
 open(url)
